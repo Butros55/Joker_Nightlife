fca45083e89a3762027a529754ba45c96d587181
Butros55 <geret.w@web.de>
2023-12-15 15:33:34 +0100

    making of secong reserve screen

--- Items/VIPBookingItems.js
+++ Items/VIPBookingItems.js
@@ -3,18 +3,26 @@ export default [
         id: '1',
         name: 'VIP Lounge - Mainhall',
         music: 'RNG, HipHop, Dance',
-        imageHome : require('../assets/pictures/VIP/main.jpeg'),
+        imageHome : require('../assets/pictures/VIP/main/main.jpeg'),
+        images: [
+            require('../assets/pictures/VIP/main/main.jpeg'),
+            require('../assets/pictures/VIP/main/main1.jpg'),
+            require('../assets/pictures/VIP/main/main2.jpg')
+        ],
+        description: 'Reisen sie in der Zeit zurück. In unserer VIP Mainhall Lounge können sie die 80er Jahre neu erleben. Das Interior erinnert an ein damaliges Bistro aus Amerika.'
     },
     {
         id: '2',
         name: 'Cocktailbar',
         music: 'Hardstyle, HipHop',
         imageHome : require('../assets/pictures/Bereiche/cocktailbar.jpg'),
+        description: 'Unsere Coctailbar ist ............................................................................................................................................................................'
     },
     {
         id: '3',
         name: 'Jolly',
         music: 'Pop, HipHop, RNG',
         imageHome : require('../assets/pictures/Bereiche/jolly/jolly.jpg'),
+        description: 'Unser Jollybereich ist ............................................................................................................................................................................'
     },
 ];
--- screens/VIPBook.js
+++ screens/VIPBook.js
@@ -8,51 +8,99 @@ import {
   TouchableOpacity,
   UIManager,
   View,
-  Image
+  Image,
+  Dimensions
 } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
 import themeContext from '../context/themeContext';
 import list from '../Items/VIPBookingItems'
-import {Calendar, LocaleConfig} from 'react-native-calendars';
+import Carousel from 'react-native-reanimated-carousel';
+
+const width = Dimensions.get('window').width;
 
 const VIPBook = ({route, navigation}) => {
 
     const { item, date } = route.params;
     const theme = useContext(themeContext)
+    const [expanded, setExpanded] = useState(false);
+    const [selected, setSelected] = useState();
+    const [buttonColor, setButtonColor] = useState('#f2f2f2')
+    const [textColor, setTextColor] = useState('rgb(0,0,0)')
 
   return (
-    <View style={{flex: 1}}>
-      <View style={{ alignItems: 'center', top: '7.6%'}}>
-        <Text style={{fontSize: 17, fontWeight: 500, color: theme.text}}>Reservierung</Text>
-      </View>
-      <View></View>
-      {/* back button */}
-      <TouchableOpacity
-        onPress={() => {[navigation.goBack()]}}
-        style={{alignSelf: 'flex-start', paddingLeft: 30, top: 45, height: 60, width: 60}}
-      >
-        <Text>
-          <Icon
-            type='font-awesome'
-            name='chevron-left'
-            size={30}
-            color={theme.text}
-          />
-        </Text>
-      </TouchableOpacity>
-      <View>
-        <Text
-          style={{fontWeight: 500, fontSize: 35, alignSelf: 'center', paddingTop: 30}}
+    <GestureHandlerRootView style={{flex: 1, backgroundColor: theme.background}}>
+      <ScrollView>
+        <View style={{ alignItems: 'center', top: 67}}>
+          <Text style={{fontSize: 17, fontWeight: 500, color: theme.text}}>Reservierung</Text>
+        </View>
+        <View></View>
+        {/* back button */}
+        <TouchableOpacity
+          onPress={() => {[navigation.goBack()]}}
+          style={{alignSelf: 'flex-start', paddingLeft: 30, top: 45, height: 60, width: 60}}
         >
-          {item.name}
-        </Text>
-        <Text
-          style={{fontSize: 20, fontWeight: 300, alignSelf: 'center', top: '50%'}}
+          <Text>
+            <Icon
+              type='font-awesome'
+              name='chevron-left'
+              size={30}
+              color={theme.text}
+            />
+          </Text>
+        </TouchableOpacity>
+        <View
+          style={{paddingTop:50}}
         >
-          Reserviervdatum: {date}
-        </Text>
-      </View>
-    </View>
+          <Text
+            style={{fontWeight:400, fontSize: 20, paddingHorizontal: 30, paddingBottom: 5}}
+          >
+            {item.name}
+          </Text>
+          <Text
+            style={{fontWeight:200, fontSize: 15, paddingHorizontal: 30}}
+          >
+            {item.description}
+          </Text>
+          <View style={{top: 40, paddingHorizontal: 20}}>
+
+          </View>
+        </View>
+        <Carousel
+                loop={true}
+                width={width}
+                height={width / 1.5}
+                autoPlay={true}
+                data={item.images}
+                scrollAnimationDuration={1000}
+                autoPlayInterval={5000}
+                mode='default'
+                renderItem={({ items }) => (
+                    <View
+                    style={{
+                        flex: 1,
+                        justifyContent: 'center',
+                        borderRadius: 25,
+                    }}
+                    >
+                        <TouchableOpacity
+                            onPress={() => {[openEvents( item ), ModalRef.current?.present()]}}
+                            style={{ height: '85%'}}
+                            >
+                            <Image source={items} style={[styles.image, { width: '85%', borderRadius: 25}]} />
+                        </TouchableOpacity>
+
+                    </View>
+                )}
+            />
+            <Button
+              title={'Reservierung abschließen'}
+              titleStyle={{color: theme.text}}
+              style={{paddingVertical: 10}}
+              buttonStyle={[styles.button, {backgroundColor: theme.button}]}
+              onPress={() => console.log(item)}
+            />
+      </ScrollView>
+    </GestureHandlerRootView>
   )
 }
 
@@ -61,7 +109,9 @@ export default VIPBook
 const styles = StyleSheet.create({
     button: {
       height: 45,
-      borderRadius: 25
+      borderRadius: 25,
+      width: '90%',
+      alignSelf: 'center'
     },
     imageHome: {
       height: 220,
